7ba907a40b22feea5f42dbf8eb19b015ad190014
root <root@Didi.localdomain>
2025-01-21 11:22:37 +0100

    add some files

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
+
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Ensure each tuple has at least 2 elements by adding (0, 0) as defaults
     tuple_a = tuple_a + (0, 0)
     tuple_b = tuple_b + (0, 0)
-    
-    # Use only the first two elements from each tuple
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
