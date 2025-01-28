6b09c641d60f9a14b00adfd0ec933e7c366968cf
root <root@Didi.localdomain>
2025-01-28 11:24:19 +0100

    add some files

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -8,8 +8,8 @@ class Square:
     def __init__(self, size):
         """Initialize the square with a given size.
 
+
         Args:
             size: The size of the square.
         """
         self.__size = size
-        
