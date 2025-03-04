b3b9f88f86bdd6cf2d66709060fcdf59032652d5
root <root@Didi>
2025-03-04 11:09:22 +0100

    add some files

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,16 +1,2 @@
--- Supprimer "George" de la table
-DELETE FROM second_table WHERE name = 'George';
-
--- Ajouter trois enregistrements pour "Aria"
-INSERT INTO second_table (score, name)
-VALUES (18, 'Aria');
-INSERT INTO second_table (score, name)
-VALUES (18, 'Aria');
-INSERT INTO second_table (score, name)
-VALUES (12, 'Aria');
-
--- Sélectionner les résultats, afficher le score et le nom, triés par score décroissant
-SELECT score, name
-FROM second_table
-WHERE name IS NOT NULL
-ORDER BY score DESC;
+-- script that lists all records of the table second_table of the db hbtn_0c_0
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,2 @@
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-);
+-- Script that creates a table called first_table
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
